7243d5fa6b5e5519db04535ab45174d6ccf71d9f
Itumeleng <217045761@student.uj.ac.za>
2021-04-02 09:24:55 +0200

    started upgraded app

--- touch_point_click_service_provider/lib/src/components/phoneNumEditText.dart
+++ touch_point_click_service_provider/lib/src/components/phoneNumEditText.dart
@@ -25,18 +25,27 @@ class _PhoneNumEditTextState extends State<PhoneNumEditText> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        countryWithCode(),
-        Flexible(
-          child: UtilWidget.txtInputText(
-            "071 456 7892",
-            null,
-            _controller,
-            TextInputType.phone,true
+    return txtInputText();
+  }
+
+  Widget txtInputText() {
+    return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      child: Row(
+        children: [
+          countryWithCode(),
+          Flexible(
+            child: TextField(
+              controller: _controller,
+              enabled: true,
+              keyboardType: TextInputType.phone,
+              style: TextStyle(fontSize: 20, height: 1.0),
+              decoration: UtilWidget.txtInputDecor("071 456 7892", null, null),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
@@ -52,28 +61,35 @@ class _PhoneNumEditTextState extends State<PhoneNumEditText> {
   }
 
   Widget countryWithCode() {
-    return Container(
-      width: null,
-      child: GestureDetector(
-        onTap: () => _showCountryPicker(),
-        child: Row(
-          children: [
-            Image.asset(
-              _country.flag,
-              package: countryCodePackageName,
-              width: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 4.0),
-              child: Text(
-                _country == null ? '' : '(${_country?.callingCode ?? '+code'})',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18,
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0),
+      child: Container(
+        width: null,
+        child: GestureDetector(
+          onTap: () => _showCountryPicker(),
+          child: Row(
+            children: [
+              Image.asset(
+                _country.flag,
+                package: countryCodePackageName,
+                width: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 4.0),
+                child: Text(
+                  _country == null
+                      ? ''
+                      : '(${_country?.callingCode ?? '+code'})',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
               ),
-            ),
-          ],
+              Icon(Icons.arrow_drop_down_sharp,
+                  color: Colors.black, size: 24.0),
+            ],
+          ),
         ),
       ),
     );
